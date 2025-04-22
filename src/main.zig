e653e9a74b44c237bd30dd25a7467ca0f5f15036
Brian <bphilip777@gmail.com>
2025-04-22 14:54:57 -0600

    Updated test files need to add codingcase

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
+const trimRight = std.mem.trimRight;
+const eql = std.mem.eql;
 const TextData = @import("TextData.zig");
 
 // pub fn main() !void {
@@ -15,83 +17,12 @@ const TextData = @import("TextData.zig");
 //     try text.parse();
 // }
 
-pub fn main() !void {
-    var da = std.heap.DebugAllocator(.{}){};
-    const allo = da.allocator();
-    defer std.debug.assert(.ok == da.deinit());
-
-    const src_dir = "src/test/src/";
-    const dst_dir = "src/test/dst/";
-    const exp_dir = "src/test/exp/";
-    const ext = ".zig";
-
-    const filepaths = [_][]const u8{
-        "inline_fn_vk",
-        // "inline_fn",
-        // "extern",
-        // "extern_fn_vk",
-        // "extern_fn",
-        // "extern_var",
-        // "extern_const",
-        // "export_var",
-        // "fn",
-        // "extension_name",
-        // "spec_version",
-        // "type_name",
-        // "pfn",
-        // "import",
-        // "opaque",
-        // "opaque_vk",
-        // "extern_union_vk",
-        // "extern_union",
-        // "extern_struct_vk",
-        // "extern_struct",
-        // "compile_error",
-        // "enum",
-        // "flag1",
-        // "flag2",
-        // "type",
-        // "type_vk",
-        // "all",
-    };
-
-    for (filepaths) |filepath| {
-        const src_path = try std.fmt.allocPrint(allo, "{s}{s}{s}", .{ src_dir, filepath, ext });
-        defer allo.free(src_path);
-
-        const dst_path = try std.fmt.allocPrint(allo, "{s}{s}{s}", .{ dst_dir, filepath, ext });
-        defer allo.free(dst_path);
-
-        var text = try TextData.init(allo, src_path, dst_path);
-        defer text.deinit();
-        try text.parse();
-
-        // load files to compare results
-        const dfile = try std.fs.cwd().openFile(dst_path, .{});
-        defer dfile.close();
-        const ddata = try dfile.readToEndAlloc(allo, 1 * 1024 * 1024);
-        defer allo.free(ddata);
-        print("{s}\n", .{ddata});
-
-        const exp_path = try std.fmt.allocPrint(allo, "{s}{s}{s}", .{ exp_dir, filepath, ext });
-        defer allo.free(exp_path);
-        const efile = try std.fs.cwd().openFile(exp_path, .{});
-        defer efile.close();
-        const edata = try efile.readToEndAlloc(allo, 1 * 1024 * 1024);
-        defer allo.free(edata);
-        print("{s}\n", .{edata});
-
-        const match = std.mem.eql(u8, ddata, edata);
-
-        print("Matches: {}\n", .{match});
-    }
-}
-
-test "Parse different text files" {
+test "Processing Vulkan File" {
     const allo = std.testing.allocator;
 
     const src_dir = "src/test/src/";
     const dst_dir = "src/test/dst/";
+    const exp_dir = "src/test/exp/";
     const ext = ".zig";
 
     const filepaths = [_][]const u8{
@@ -115,13 +46,17 @@ test "Parse different text files" {
         "extern_union",
         "extern_struct_vk",
         "extern_struct",
+        "compile_error",
+        "enum",
         "flag1",
         "flag2",
         "type",
-        "unknown",
+        "type_vk",
         "all",
     };
 
+    var dbuffer: [1024]u8 = undefined;
+    var ebuffer: [1024]u8 = undefined;
     for (filepaths) |filepath| {
         const src_path = try std.fmt.allocPrint(allo, "{s}{s}{s}", .{ src_dir, filepath, ext });
         defer allo.free(src_path);
@@ -132,7 +67,23 @@ test "Parse different text files" {
         var text = try TextData.init(allo, src_path, dst_path);
         defer text.deinit();
         try text.parse();
-    }
 
-    try std.testing.expect(true);
+        const dfile = try std.fs.cwd().openFile(dst_path, .{});
+        defer dfile.close();
+        var dreader = dfile.reader();
+
+        const exp_path = try std.fmt.allocPrint(allo, "{s}{s}{s}", .{ exp_dir, filepath, ext });
+        defer allo.free(exp_path);
+        const efile = try std.fs.cwd().openFile(exp_path, .{});
+        defer efile.close();
+        var ereader = efile.reader();
+
+        var dline: []const u8 = undefined;
+        var eline: []const u8 = undefined;
+        while (true) {
+            dline = if (try dreader.readUntilDelimiterOrEof(&dbuffer, '\n')) |line| trimRight(u8, line, "\r\n") else break;
+            eline = if (try ereader.readUntilDelimiterOrEof(&ebuffer, '\n')) |line| trimRight(u8, line, "\r\n") else break;
+            try std.testing.expectEqualStrings(dline, eline);
+        }
+    }
 }
--- src/test/dst/all.zig
+++ src/test/dst/all.zig
@@ -173,7 +173,7 @@ pub const DWORD64 = c_ulonglong;
 pub const Bool32 = enum(u32) {
     false = 0,
     true = 1,
- };
+};
 pub const DeviceAddress = u64;
 pub const DeviceSize = u64;
 pub const Flags = u32;
