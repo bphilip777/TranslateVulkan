e9cf25a439d78b7513e2ed2136d594227fb2188e
Brian <bphilip777@gmail.com>
2025-04-26 21:16:21 -0600

    Added test for edge case to extern fn vk

--- src/test/dst/extern_fn_vk.zig
+++ src/test/dst/extern_fn_vk.zig
@@ -1,2 +1,3 @@
 pub const PES = @import("PackedEnumSet").PackedEnumSet;
 pub extern fn createWin32SurfaceKhr(instance: Instance, p_create_info: [*c]const Win32SurfaceCreateInfoKHR, p_allocator: [*c]const AllocationCallbacks, p_surface: [*c]SurfaceKHR) Result;
+pub extern fn copyMemoryToImage(device: Device, pcopy_memory_to_image_info: [*c]const copyMemoryToImageInfo) Result;
--- src/test/src/extern_fn_vk.zig
+++ src/test/src/extern_fn_vk.zig
@@ -1,1 +1,2 @@
 pub extern fn vkCreateWin32SurfaceKHR(instance: VkInstance, pCreateInfo: [*c]const VkWin32SurfaceCreateInfoKHR, pAllocator: [*c]const VkAllocationCallbacks, pSurface: [*c]VkSurfaceKHR) VkResult;
+pub extern fn vkCopyMemoryToImage(device: VkDevice, pCopyMemoryToImageInfo: [*c]const VkCopyMemoryToImageInfo) VkResult;
